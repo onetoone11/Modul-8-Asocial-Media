7f250a13b1ebc5e0da9c72b26536090b98647cc7
Matilda Jönsson <matilda.jonsson4@learnet.se>
2022-02-02 11:54:41 +0100

    d

--- M8/resources/js/components/Navbar.js
+++ M8/resources/js/components/Navbar.js
@@ -1,8 +1,12 @@
 import Button from './Button'
 import Logo from './logoBlack.png'
+import React from 'react'
 
 
 export default function Navbar(){
+
+    const [loggedIn, setLoggedIn] = React.useState(true);
+
     return(
         <nav className="d-flex justify-content-between align-items-center p-0">
             <div className="nav--logo"><img className="logo--image" src={Logo}></img></div>
@@ -13,10 +17,10 @@ export default function Navbar(){
             </form>
             
 
-            <div className="nav--account">
+            {loggedIn ? <div className="nav--account">
                 <button className="mr-3 btn-login bg--light">Log In</button>
                 <button className="btn-register">Register</button>
-            </div>
+            </div> : <h1>Hello</h1>}
         </nav>
     )
 }
--- M8/resources/js/components/Profile.js
+++ M8/resources/js/components/Profile.js
@@ -1,10 +1,24 @@
-
+import findus from './findus.png'
 
 export default function Profile(){
     return(
         <>
-            <div>
-                <div className="profile--img"></div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-3"></div>
+
+                    <div className="col-lg-6">
+                        <div className="profile--img"></div>
+
+                        <div className="row">
+                            <div className="col-lg-10 profile--create-post"></div>
+                            <div className="col-lg-2 profile--settings"></div>
+                        </div>
+
+                    </div>
+
+                    <div className="col-lg-3"></div>
+                </div>   
             </div>
         </>
     )
