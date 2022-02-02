48d8bbbc2ddd60946f055f0eb34414f9cc5f2bd8
Matilda Jönsson <matilda.jonsson4@learnet.se>
2022-02-02 14:38:17 +0100

    css

--- M8/resources/js/components/Login.js
+++ M8/resources/js/components/Login.js
@@ -9,7 +9,6 @@ export default function Login(){
             
             <div className="card">
                 <div className={`card-body bg--light`}>
-                {/* <div className="card-body"> */}
                     <h2 className="card-title">Log in</h2>
                     <hr/>
 
