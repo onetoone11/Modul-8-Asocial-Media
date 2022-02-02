f0f39ad125a436da038e971c7a14716944523852
Ella Ahlstedt <ella.ahlstedt@learnet.se>
2022-02-02 10:56:29 +0100

    Log in Done

--- M8/resources/js/index.js
+++ M8/resources/js/index.js
@@ -1,12 +1,9 @@
 import Example from './components/Example'
-<<<<<<< HEAD
 import Navbar from './components/Navbar'
 import Start from './components/Start'
 import Login from './components/Login'
-=======
 
 import App from './app'
->>>>>>> 857b2a5796b868dfec17b7b74eb90855b54b055c
 import React from 'react'
 import ReactDOM from 'react-dom'
 import '../css/bootstrap.css'
